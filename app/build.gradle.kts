0607027f5fdca875944d61185606d1f2dc1b2c27
Teja Karlapudi <teja2495@gmail.com>
2025-07-30 17:18:37 -0400

    feat: update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.tk.daystrack"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
