747a8c23bc2b2ecfe88310273e68130a511d82e9
Teja Karlapudi <teja2495@gmail.com>
2026-04-27 23:13:34 -0400

    Bump version code to 10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.tk.daystrack"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
+        versionCode = 10
         versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
